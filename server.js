28cbe229ba6c1986e25d2419dcf8334a4fc16660
Ken Hoff <thekenhoff@gmail.com>
2015-07-04 15:48:24 -0700

    good start

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-apiToken = "xoxb-7232792789-hyoYDRsPRkTKmLyIQbkYkGNp"
+// TODO
+// remove punctuation
+// ignore blank replies
+
+slackToken = "xoxb-7232792789-hyoYDRsPRkTKmLyIQbkYkGNp"
+giphyToken = "dc6zaTOxFJmzC"
 
 Slack = require("slack-client")
+giphy = require("giphy")(giphyToken)
 
-slack = new Slack(apiToken, true, true)
+slack = new Slack(slackToken, true, true)
 
 slack.on("open", function () {
     console.log("opened!")
-    // console.log(slack.channels)
+    // console.log(slack)
 })
 
 
@@ -28,11 +34,39 @@ isDirect = function(userId, messageText) {
 
 
 slack.on("message", function (message) {
+    console.log(message)
     var channel = slack.getChannelGroupOrDMByID(message.channel);
     var user = slack.getUserByID(message.user);
-    if (message.type === 'message' && isDirect(slack.self.id, message.text)) {
-        console.log (message.text)
+    // console.log(user)
+    if (message.type === 'message' && !("subtype" in message) && isDirect(slack.self.id, message.text)) {
+        // console.log (message.text)
         // channel.send("hello!")
+
+        searchTerms = message.text.replace(makeMention(slack.self.id), "")
+
+        // channel.send("i'm gonna try to find a gif with the terms: " + searchTerms)
+
+        // 1. grab a random gif from the giphy api
+
+        giphy.random({tag: searchTerms, rating: "pg-13"}, function (err, gif, res) {
+
+            console.log(gif)
+
+            // 2. direct message it to the user
+
+            dm = slack.getDMByName(user.name)
+
+            dm.send("> How does this one look?")
+            dm.send("> " + gif.data.url)
+
+            // dm.send("hello!")
+
+        })
+
+        // 3. if the user responds "yes", post the gif to the channel
+        // 4. if the user responds "no", goto step 1
+
+
     }
 
 })
