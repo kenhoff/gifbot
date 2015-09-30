db6cdda72de1e4251871f937f6ebbc894055d0cf
Ken Hoff <thekenhoff@gmail.com>
2015-09-29 18:19:12 -0700

    output for when a slack token isn't found

--- run.js
+++ run.js
@@ -1,2 +1,11 @@
 Gifbot = require("../gifbot")
-newGifbot = new Gifbot("xoxb-7297989824-GaGdVUqaKl0mgYSaOC9x99MI")
+console.log(process.env.SLACK_TOKEN)
+if (process.env.SLACK_TOKEN) {
+	newGifbot = new Gifbot(process.env.SLACK_TOKEN)
+} else {
+	console.log("No Slack token found in process.env.SLACK_TOKEN!!!");
+	console.log("Run gifbot with a command like:")
+	console.log("================================================")
+	console.log("SLACK_TOKEN=your-slack-token-here nodemon run.js")
+	console.log("================================================")
+}
