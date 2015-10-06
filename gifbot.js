a32366d821d9fc9ab24806ca7d703f1e6c000928
Ken Hoff <thekenhoff@gmail.com>
2015-10-05 19:18:07 -0700

    re-integration phase 1

--- gifbot.js
+++ gifbot.js
@@ -8,16 +8,71 @@ giphy = require("giphy")(giphyToken)
 var events = require('events');
 
 
-Gifbot = function (slackToken) {
-	// see link below...
+Gifbot = function(slackToken) {
+
+	this.users = {}
+		// see link below...
 	events.EventEmitter.call(this)
 	this.slackToken = slackToken
 	this.slack = new Slack(slackToken, false, true)
 
 	this.slack.login()
-	this.slack.on("error", function (err) {
+	this.slack.on("error", function(err) {
 		this.emit("error", err)
 	}.bind(this))
+
+	this.dmGif = function(searchTerms, userId, channelId) {
+		giphy.random({
+			tag: searchTerms,
+			rating: "pg-13"
+		}, function(err, gif, res) {
+			// 2. direct message it to the user
+			dm = this.slack.getDMByName(this.slack.getUserByID(userId).name)
+			dm.send("> How does this one look? \n> " + gif.data.url)
+			users[userId] = {
+					"search": searchTerms,
+					"latestGif": gif.data.url,
+					"channelId": channelId
+				}
+				// console.log(users);
+		})
+	}
+
+	this.slack.on("message", function(message) {
+		// console.log(message);
+		// console.log(this.users)
+		var channel = this.slack.getChannelGroupOrDMByID(message.channel);
+		var user = this.slack.getUserByID(message.user);
+		// console.log(channel)
+
+
+		// If a user mentions gifbot in a channel...
+		if (message.type === 'message' && !("subtype" in message) && isDirect(this.slack.self.id, message.text)) {
+			searchTerms = message.text.replace(makeMention(this.slack.self.id), "")
+				// 1. grab a random gif from the giphy api
+			this.dmGif(searchTerms, user.id, channel.id)
+				// 3. if the user responds "yes", post the gif to the channel
+				// 4. if the user responds "no", goto step 1
+		}
+
+
+
+		// If the user isn't gifbot...
+		// ignore message events sent by posting to the DM channel
+		else if ((message.user != this.slack.self.id) && !("subtype" in message)) {
+			if (this.users[message.user] == null) {
+				return
+			} else if (message.text == "yes") {
+				// console.log("posting gif");
+				// post the latest gif for the user into the channel
+				this.slack.getChannelGroupOrDMByID(this.users[message.user].channelId).send(this.users[message.user].latestGif)
+			} else {
+				this.dmGif(this.users[message.user].search, user.id, this.users[message.user].channelId)
+			}
+		}
+
+	}.bind(this))
+
 }
 
 // ???
@@ -28,7 +83,7 @@ module.exports = Gifbot
 
 
 
-// slack.on("open", function() {
+// this.slack.on("open", function() {
 // 	// console.log("opened!")
 // 	// console.log(slack)
 // })
@@ -44,52 +99,3 @@ isDirect = function(userId, messageText) {
 
 	return (messageText.indexOf(userTag) != -1)
 };
-
-
-// function dmGif(searchTerms, userId, channelId) {
-// 	giphy.random({
-// 		tag: searchTerms,
-// 		rating: "pg-13"
-// 	}, function(err, gif, res) {
-// 		// 2. direct message it to the user
-// 		dm = slack.getDMByName(slack.getUserByID(userId).name)
-// 		dm.send("> How does this one look? \n> " + gif.data.url)
-// 		users[userId] = {
-// 			"search": searchTerms,
-// 			"latestGif": gif.data.url,
-// 			"channelId": channelId
-// 		}
-// 		// console.log(users);
-// 	})
-// }
-//
-// slack.on("message", function(message) {
-// 	// console.log(message);
-// 	// console.log(users)
-// 	var channel = slack.getChannelGroupOrDMByID(message.channel);
-// 	var user = slack.getUserByID(message.user);
-// 	// console.log(channel)
-// 	if (message.type === 'message' && !("subtype" in message) && isDirect(slack.self.id, message.text)) {
-// 		searchTerms = message.text.replace(makeMention(slack.self.id), "")
-// 			// 1. grab a random gif from the giphy api
-// 		dmGif(searchTerms, user.id, channel.id)
-// 			// 3. if the user responds "yes", post the gif to the channel
-// 			// 4. if the user responds "no", goto step 1
-// 	}
-//
-//
-// 	// ignore message events sent by posting to the DM channel
-// 	else if ((message.user != slack.self.id) && !("subtype" in message)) {
-// 		// console.log("got a dm");
-// 		if (message.text == "yes") {
-// 			// console.log("posting gif");
-// 			// post the latest gif for the user into the channel
-// 			slack.getChannelGroupOrDMByID(users[message.user].channelId).send(users[message.user].latestGif)
-// 		} else {
-// 			dmGif(users[message.user].search, user.id, users[message.user].channelId)
-// 		}
-// 	}
-//
-// })
-//
-// users = {}
