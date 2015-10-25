951674b12c44de8b47d966f91b879b91416c9b06
Ken Hoff <thekenhoff@gmail.com>
2015-10-25 15:11:32 -0700

    some better logging, helping with debugging

--- gifbot.js
+++ gifbot.js
@@ -13,8 +13,13 @@ Gifbot = function(slackToken) {
 	this.slack = new Slack(slackToken, false, true)
 
 	this.slack.login()
+
+	this.slack.on("open", function () {
+		console.log("Connected at", new Date());
+	})
+
 	this.slack.on("error", function(err) {
-		// console.log(err);
+		console.log(err);
 		msg = err
 		if (err == "invalid_auth") {
 			msg = "Invalid slack API token: " + this.slackToken
@@ -33,9 +38,11 @@ Gifbot = function(slackToken) {
 			// 2. direct message it to the user
 			dm = this.slack.getDMByName(this.slack.getUserByID(userId).name)
 			if (gif.data.length == 0) {
+				console.log("Gifbot couldn't find any gifs for", userId, "in", channelId, "with search terms:", searchTerms);
 				dm.send("I couldn't find any gifs of `" + searchTerms + "` :(")
 			}
 			else {
+				console.log("Gifbot suggested", gif.data.url, "for", userId, "in", channelId, "with search terms:", searchTerms);
 				dm.send("> How does this one look?\n> " + gif.data.url)
 				this.users[userId] = {
 					"search": searchTerms,
@@ -52,6 +59,7 @@ Gifbot = function(slackToken) {
 		// If a user mentions gifbot in a channel...
 		if (message.type === 'message' && !("subtype" in message) && isMention(this.slack.self.id, message.text)) {
 			searchTerms = message.text.replace(makeMention(this.slack.self.id), "")
+			console.log(message.user, "requested a gif for", searchTerms, "in", message.channel);
 			this.dmGif(searchTerms, user.id, channel.id)
 		}
 		// If the user isn't gifbot...
@@ -60,9 +68,11 @@ Gifbot = function(slackToken) {
 				return
 			} else if (sentiment(message.text).score > "0") {
 				// post the latest gif for the user into the channel
+				console.log(message.user, "accepted and posted gif", this.users[message.user].latestGif, "to", message.channel, "with search terms:", searchTerms);
 				this.slack.getChannelGroupOrDMByID(this.users[message.user].channelId).send(this.users[message.user].latestGif)
 				delete this.users[message.user]
 			} else if (sentiment(message.text).score < "0") {
+				console.log(message.user, "rejected gif", this.users[message.user].latestGif, "for", message.channel, "with search terms:", searchTerms);
 				this.dmGif(this.users[message.user].search, user.id, this.users[message.user].channelId)
 			}
 			else {
